f0898bf010e308ba5a3d5db8dbd20a328ecdcc2e
Alex Yuan <36246959+alexwhyy@users.noreply.github.com>
2021-05-16 14:18:06 -0400

    Update 17. Letter Combinations of a Phone Number

--- 17. Letter Combinations of a Phone Number/solution.py
+++ 17. Letter Combinations of a Phone Number/solution.py
@@ -1,27 +1,22 @@
 class Solution:
-    mappings = {
-        "2": ["a", "b", "c"],
-        "3": ["d", "e", "f"],
-        "4": ["g", "h", "i"],
-        "5": ["j", "k", "l"],
-        "6": ["m", "n", "o"],
-        "7": ["p", "q", "r", "s"],
-        "8": ["t", "u", "v"],
-        "9": ["w", "x", "y", "z"],
-    }
-
-    def traverse(self, digits):
-        digit = digits[0]
-        if len(digits) == 1:
-            return set(self.mappings[digit])
-        prev = self.traverse(digits[1:])
-        new = set()
-        for i in prev:
-            for j in self.mappings[digit]:
-                new.add(j + i)
-        return new
-
     def letterCombinations(self, digits: str) -> List[str]:
         if len(digits) == 0:
             return []
-        return self.traverse(digits)
+        mappings = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        ans = set(mappings[digits[0]])
+        for digit in digits[1:]:
+            new = set()
+            for comb in ans:
+                for letter in mappings[digit]:
+                    new.add(comb + letter)
+            ans = new
+        return ans
