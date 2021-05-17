adbb299c16475030f61e5232b73a9ac98dd3c541
Alex Yuan <36246959+alexwhyy@users.noreply.github.com>
2021-05-16 21:42:53 -0400

    Update 1. Two Sum

--- 1. Two Sum/solution.py
+++ 1. Two Sum/solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        history = {}
         for i, num in enumerate(nums):
-            if target - num in nums and nums.index(target - num) != i:
-                return [i, nums.index(target - num)]
+            if target - num in history:
+                return [i, history[target - num]]
+            history[num] = i
