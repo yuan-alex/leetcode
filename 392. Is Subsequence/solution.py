5cd77fc2ee05917ec52d7fcd1a8928e64ec87fa1
Alex Yuan <36246959+alexwhyy@users.noreply.github.com>
2021-05-21 01:26:08 -0400

    Add 392. Is Subsequence

---
+++ 392. Is Subsequence/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        si = 0
+        ti = 0
+        while si < len(s) and ti < len(t):
+            if s[si] == t[ti]:
+                si += 1
+            ti += 1
+        return si == len(s)
