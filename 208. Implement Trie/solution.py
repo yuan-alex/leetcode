60f3e49c1ea8c7258f337a813496ee4222d1f425
Alex Yuan <alex@alexyuan.me>
2023-03-16 22:19:43 -0400

    208 python

---
+++ 208. Implement Trie/solution.py
@@ -0,0 +1,35 @@
+class Trie:
+
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        node = self.trie
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node["END"] = True
+
+    def search(self, word: str) -> bool:
+        node = self.trie
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return "END" in node
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.trie
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
