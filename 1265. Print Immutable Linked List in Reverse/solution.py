ad41c820a8c65f105720338bee84951dcfe81b3f
Alex Yuan <alex@alexyuan.me>
2023-03-30 21:22:46 -0400

    1265 python

---
+++ 1265. Print Immutable Linked List in Reverse/solution.py
@@ -0,0 +1,16 @@
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        def recursive(node):
+            if node is None:
+                return
+            recursive(node.getNext())
+            node.printValue()
+        recursive(head)
